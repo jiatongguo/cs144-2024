ac4ce9a433b903c6ac30739e7cab28533241462a
jiatongguo <130060359+jiatongguo@users.noreply.github.com>
2024-05-01 20:11:13 +0800

    the first time passing all test cases

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,57 +1,66 @@
 #include "reassembler.hh"
 
-void Reassembler::insert( uint64_t first_index, std::string data, bool is_last_substring )
+void Reassembler::insert(uint64_t first_index, std::string data, bool is_last_substring)
 {
-    if (data.empty() )
-    {
-        if (is_last_substring == true and output_.writer().bytes_pushed() == first_index)
-            output_.writer().close();
-        return;
-    }
-
-    if (is_last_substring == true)
+    if ( is_last_substring == true )
     {
         is_last_ = true;
-        end_mark_ = first_index + data.size();
+        end_index_ = first_index + data.size();
+    }
+
+    if ( data.empty() )
+    {
+        if (is_last_substring == true and output_.writer().bytes_pushed() == first_index)
+        {
+            output_.writer().close();
+        }
+        return;
     }
 
     //stream is closed or capacity is not enough
     if (output_.writer().is_closed() == true or output_.writer().available_capacity() == 0)
+    {
         return;
+    }
 
     first_unassembled_index_ = output_.writer().bytes_pushed();
     first_unaccepted_index_ = first_unassembled_index_ + output_.writer().available_capacity();
 
     //out of range
-    if (first_index + data.size() <=  first_unassembled_index_ or first_index >= first_unaccepted_index_)
+    if ( first_index + data.size() <= first_unassembled_index_ or first_index >= first_unaccepted_index_ )
+    {
         return;
+    }
 
-    //handle overlapping with has pushed
-    if (first_index < first_unassembled_index_)
+    //handle overlapping with pushed
+    if ( first_index < first_unassembled_index_ )
     {
         data.erase(0, first_unassembled_index_ - first_index);
         first_index = first_unassembled_index_;
     }
 
     //remove the unaccepted bytes
-    if (first_index + data.size() >= first_unaccepted_index_)
+    if ( first_index + data.size() >= first_unaccepted_index_ )
     {
         data.resize(first_unaccepted_index_ - first_index);
         is_last_substring = false; //not fininshed
     }
 
     auto last_index = first_index + data.size() - 1; //the right bound
-    auto left_it = buffer_map_.lower_bound(first_index);
+
     //handle the left overlapping
-    if (left_it != buffer_map_.begin() ) //not the first element but can be not found(return the end iter)
+    auto left_it = buffer_map_.upper_bound( first_index );
+    if ( left_it != buffer_map_.begin() ) //not the first element but can be not found(return the end iter)
     {
         auto prev = std::prev(left_it);
         //left overlapping
-        if (prev->first + prev->second.size() - 1 >= first_index)
+        if ( prev->first + prev->second.size() - 1 >= first_index )
         {
             //data is fully overlapped
-            if (prev->first + prev->second.size() - 1 >= last_index)
+            if ( prev->first + prev->second.size() - 1 >= last_index )
+            {
                 return;
+            }
 
             data = prev->second.substr(0, first_index - prev->first) + data; //merge
             first_index = prev->first;
@@ -60,81 +69,48 @@ void Reassembler::insert( uint64_t first_index, std::string data, bool is_last_s
         }
     }
 
-    else
-    { //is the first position
-        if (not buffer_map_.empty() )
-        {
-            if (first_index == left_it->first and last_index <= left_it->first + left_it->second.size() - 1)
-                return;
-        }
-    }
     //handle right overlapping
-    auto right_it = buffer_map_.lower_bound(last_index);
-    if (right_it != buffer_map_.begin() )
+    auto right_it = buffer_map_.upper_bound( last_index );
+    if ( right_it != buffer_map_.begin() )
     {
-        auto prev = std::prev(right_it);
-        if (last_index <= prev->first + prev->second.size() - 1)
+        auto prev = std::prev( right_it );
+        if ( last_index <= prev->first + prev->second.size() - 1 )
         {
-            data += prev->second.substr(last_index - prev->first);
+            data += prev->second.substr( last_index - prev->first + 1 ); //!!
             last_index = prev->first + prev->second.size() - 1;
         }
     }
 
-    //erase [left_it, right_it)
-    while (left_it != right_it)
+    while ( left_it != right_it )
     {
         pending_bytes_counter_ -= left_it->second.size();
-        left_it = buffer_map_.erase(left_it);
-    }
+        left_it = buffer_map_.erase( left_it );
+    } //erase [left_it, right_it)
 
-    //handle the right_it
-    if (right_it != buffer_map_.end() and right_it->first == last_index)
-    {
-        //size == 1 越界
-        if (right_it->second.size() > 1)
-        {
-            data += right_it->second.substr(1);
-            last_index = right_it->first + right_it->second.size() - 1;
-        }
-
-        pending_bytes_counter_ -= right_it->second.size();
-        buffer_map_.erase(right_it);
-    }
+    buffer_map_.emplace_hint(left_it, first_index, data);
+    pending_bytes_counter_ += data.size();
 
     //check the bytes after
-    if (first_index == first_unassembled_index_)
+    while ( not buffer_map_.empty() )
     {
-        first_unassembled_index_= last_index + 1;
-        output_.writer().push(data);
-
-        auto it2 = buffer_map_.lower_bound(first_unassembled_index_);
-        while (it2 != buffer_map_.end() )
+        auto it = buffer_map_.begin();
+        if ( it->first != output_.writer().bytes_pushed() )
         {
-            if (it2->first != output_.writer().bytes_pushed() )
-            {
-                break;
-            }
-
-            else
-            {
-                pending_bytes_counter_ -= it2->second.size();
-                output_.writer().push(it2->second);
-                it2 = buffer_map_.erase(it2);
-            }
+            break;
         }
+
+        pending_bytes_counter_ -= it->second.size();
+        output_.writer().push( it->second );
+        buffer_map_.erase(it);
     }
 
-    else
+    if ( is_last_ == true and output_.writer().bytes_pushed() == end_index_ )
     {
-        buffer_map_[first_index] = data;
-        pending_bytes_counter_ += data.size();
-    }
-
-    if (is_last_ == true and output_.writer().bytes_pushed() == end_mark_)
         output_.writer().close();
+    }
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
     return pending_bytes_counter_;
-}                                                         
+}
