9ee11bef3d623dbb3cd0449bc36c7a4b991652f6
jiatongguo <janetta1204@gmail.com>
2025-08-01 16:00:48 +0000

    修复因window_size未正确更新导致FIN标志因无空间而错误设置的问题（ackno无数值，对等方发来的信息也包含窗口大小,所以先更新窗口，再检查是否ackno是否有数值

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -16,60 +16,63 @@ uint64_t TCPSender::consecutive_retransmissions() const
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  if (window_size_ == 0) // 发送窗口为0时置1
+  while (true)
   {
-    window_size_ = 1;  
+    if (window_size_ == 0) // 发送窗口为0时置1
+    {
+      window_size_ = 1;  
+    }
+
+    if (outstanding_bytes_ > window_size_)
+    {
+      return;
+    }
+    
+    TCPSenderMessage msg { make_empty_message() };
+    if (!SYN_sent_)  // 发syn包
+    {
+        msg.SYN = true;
+        SYN_sent_ = true;
+    }
+
+    size_t max_payload = std::min(TCPConfig::MAX_PAYLOAD_SIZE, 
+                            window_size_ - outstanding_bytes_ - (msg.SYN ? 1 : 0));
+
+    std::string payload;
+    while (reader().bytes_buffered() != 0 && max_payload) // 缓冲区有数据且有空间
+    { 
+          std::string_view view = reader().peek();
+          view = view.substr(0, max_payload);
+          payload += view;
+          max_payload -= payload.size();
+          input_.reader().pop(view.size()); // 从缓冲区删除
+    }
+
+    msg.payload = payload;
+
+    if (!FIN_sent_&& (window_size_ > outstanding_bytes_ + msg.sequence_length()) && reader().is_finished() )
+    {
+        FIN_sent_ = true;
+        msg.FIN  = true;
+    }
+    
+    if (msg.sequence_length() == 0) // 没有任何信息的空段
+    {
+      return;
+    }
+
+    transmit(msg);
+
+    if (timer_running_ == false)
+    {
+        timer_running_ = true; 
+        timer_elapsed_ = 0;
+    }
+
+    next_abs_seqno_ += msg.sequence_length(); 
+    outstanding_bytes_ += msg.sequence_length(); // 更新已send未ack字节数
+    outstanding_segments_.emplace(std::move(msg)); // 插入队列
   }
-
-  if (outstanding_bytes_ > window_size_)
-  {
-    return;
-  }
-
-  TCPSenderMessage msg { make_empty_message() };
-  if (!SYN_sent_)  // 发syn包
-  {
-      msg.SYN = true;
-      SYN_sent_ = true;
-  }
-
-  size_t max_payload = std::min(TCPConfig::MAX_PAYLOAD_SIZE, 
-                          window_size_ - outstanding_bytes_ - (msg.SYN ? 1 : 0));  // msg可能设置了syn
-
-  std::string payload;
-  while (reader().bytes_buffered() != 0 && max_payload) // 缓冲区有数据且有空间
-  { 
-        std::string_view view = reader().peek();
-        view = view.substr(0, max_payload);
-        payload += view;
-        max_payload -= payload.size();
-        input_.reader().pop(view.size()); // 从缓冲区删除
-  }
-
-  msg.payload = payload;
-
-  if (!FIN_sent_&& (window_size_ > outstanding_bytes_ + msg.sequence_length()) && reader().is_finished() )
-  {
-      FIN_sent_ = true;
-      msg.FIN  = true;
-  }
-  
-  if (msg.sequence_length() == 0) // 没有任何信息的空段
-  {
-    return;
-  }
-
-  transmit(msg);
-
-  if (timer_running_ == false)
-  {
-      timer_running_ = true; 
-      timer_elapsed_ = 0;
-  }
-
-  next_abs_seqno_ += msg.sequence_length(); 
-  outstanding_bytes_ += msg.sequence_length(); // 更新已send未ack字节数
-  outstanding_segments_.emplace(std::move(msg)); // 插入队列
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
@@ -79,12 +82,12 @@ TCPSenderMessage TCPSender::make_empty_message() const
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
+  window_size_ = msg.window_size; // 更新窗口
+
   if (!msg.ackno.has_value())
   {
     return;
   }
-
-  window_size_ = msg.window_size; // 更新窗口
 
   uint64_t abs_ackno {msg.ackno.value().unwrap(isn_, next_abs_seqno_)};
 
